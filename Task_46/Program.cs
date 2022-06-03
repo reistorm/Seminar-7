52778829f689497181d51b8b17b2007d7236d3f0
ELIZA <rysaeva96@mail.ru>
2022-06-03 17:18:38 +0500

    Решение 46

---
+++ Task_46/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте двумерный массив размером m x n
+// заполненный случайными числами
+
+int[,] GetArray(int a, int b) 
+{
+    int [,] array = new int[a,b];
+    Random rnd = new Random();
+    for (int i=0; i < array.GetLength(0); i++) // Обращение к первой размерности
+    {
+        for (int j = 0; i < array.GetLength(1); j++)
+        {
+            array [i,j] = rnd.Next();
+        }
+    }
+    return array;
+
+}
+
+void PrintArray(int [,] array) 
+{
+    for (int i=0; i < array.GetLength(0); i++) // Обращение к первой размерности
+    {
+        for (int j = 0; i < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] arrayResult = GetArray(5, 7);
+PrintArray(arrayResult);
+
+
